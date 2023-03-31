64cf6953f80364c76942c17ac5ac8cf7c5ebb95b
FTyumin <feodor.tjumin28@gmail.com>
2023-03-31 19:36:11 +0300

    Trying to add a button

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+from telebot import types
 from dotenv import load_dotenv
 
 
@@ -9,6 +10,8 @@ load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Hi, how are you doing?")
@@ -16,5 +19,13 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Button")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Choose what you need',reply_markup=markup)
+
 
 bot.infinity_polling()
